132cfdab8c53365eb0afb3a5dfd7361a195b0584
Mateus Talles <mateustalles@gmail.com>
2019-09-16 15:25:21 -0300

    msgSlack

---
+++ scripts/DomainInfo/msgSlack.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MESSAGE=$1
+
+if [ -z $MESSAGE ]
+then
+    echo "Ops, você precisa passar uma mensagem como argumento!"
+else    
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$MESSAGE\"}" https://hooks.slack.com/services/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
+fi
