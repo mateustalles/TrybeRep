08b6c5b3bfc697339fc4ece64f35910e2d59f30d
Mateus Talles <mateustalles@gmail.com>
2019-09-25 18:26:25 -0300

    exercício maiorValor.js

---
+++ exercises/3.3/maiorValor.js
@@ -0,0 +1,14 @@
+function maiorValor(array) {
+    var maior = 0;
+    for (let value of array) {
+        if (value > maior) {
+            maior = value;
+        }
+        else {
+            maior = maior;
+        }
+    }
+    console.log (maior);
+    return maior;
+}
+maiorValor([1,2,6,8,27,3,9]);
